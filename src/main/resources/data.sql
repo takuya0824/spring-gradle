400a1c1ad99153ea3e5af781f46bbe549312f59d
takuya0824 <thonda472@hotmail.com>
2024-07-28 16:01:27 +0900

    ログイン、ログアウトを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,9 @@ insert into brand (brand_id ,brand_name) values (1, "HONDA");
 insert into brand (brand_id ,brand_name) values (2, "KAWASAKI");
 insert into brand (brand_id ,brand_name) values (3, "SUZUKI");
 insert into brand (brand_id ,brand_name) values (4, "NISSAN");
+
+insert into user (username ,password) values ("test", "$2a$10$vj8SZHwbXcsoMHHy/0CJEepIwYj5D6SRwQv5y4mF4JUmXs7n/Cwnm");
+insert into user (username ,password) values ("admin", "$2a$10$vj8SZHwbXcsoMHHy/0CJEepIwYj5D6SRwQv5y4mF4JUmXs7n/Cwnm");
+insert into user (username ,password) values ("honda", "$2a$10$vj8SZHwbXcsoMHHy/0CJEepIwYj5D6SRwQv5y4mF4JUmXs7n/Cwnm");
+insert into user (username ,password) values ("yamada", "$2a$10$vj8SZHwbXcsoMHHy/0CJEepIwYj5D6SRwQv5y4mF4JUmXs7n/Cwnm");
+insert into user (username ,password) values ("suzuki", "$2a$10$vj8SZHwbXcsoMHHy/0CJEepIwYj5D6SRwQv5y4mF4JUmXs7n/Cwnm");
